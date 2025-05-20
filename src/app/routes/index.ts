82aa715ac23f24f9251e90d2b3e57fa144f7a396
ronymia <rony.mia7800@gmail.com>
2025-05-20 10:57:32 +0600

    feat: Implement Service, Specialization, TimeSlot, and User modules with CRUD operations
    - Added service validation schemas for creating and updating services.
    - Created specialization constants, interfaces, controllers, routes, and services for managing specializations.
    - Implemented time slot management with corresponding constants, interfaces, controllers, routes, and services.
    - Developed user management functionalities including creating admins, doctors, and patients with validation.
    - Updated routes to reflect new module structures and ensure proper authorization.
    - Enhanced error handling and response formatting across modules.

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { SpecializationRoutes } from '../modules/Specialization/specialization.route';
-import { PermissionRoutes } from '../modules/Permission/permission.route';
-import { UserRoutes } from '../modules/User/user.route';
-import { AuthRoutes } from '../modules/Auth/auth.route';
-import { ServiceRoutes } from '../modules/Service/service.route';
-import { TimeSlotRoutes } from '../modules/TimeSlot/timeSlot.route';
-import { AvailableServiceRoutes } from '../modules/AvailableService/availableService.route';
-import { AppointmentRoutes } from '../modules/Appointment/appointment.route';
+import { SpecializationRoutes } from '../modules/Specializations/specialization.route';
+import { PermissionRoutes } from '../modules/Permissions/permission.route';
+import { UserRoutes } from '../modules/Users/user.route';
+import { AuthRoutes } from '../modules/Auths/auth.route';
+import { ServiceRoutes } from '../modules/Services/service.route';
+import { TimeSlotRoutes } from '../modules/TimeSlots/timeSlot.route';
+import { AvailableServiceRoutes } from '../modules/AvailableServices/availableService.route';
+import { AppointmentRoutes } from '../modules/Appointments/appointment.route';
 
 const router = express.Router();
 
